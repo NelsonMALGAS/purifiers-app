85e01e8687f8e9ea004e3ddb553b325aff34882a
Nelson Malgas <zmalgas69@gmail.com>
2025-05-16 15:54:11 +0200

    Added SEO for app/purifiers/register/page.tsx and also created a custom not found page

--- app/purifiers/register/page.tsx
+++ app/purifiers/register/page.tsx
@@ -1,7 +1,16 @@
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+    title: "💧AquaSync | Register New Purifier",
+    description: "Easily register a new water purifier to your AquaSync account. Provide model, capacity, and installation details to keep your system up to date.",
+};
 
 const RegisterPurifier = () => {
     return (
-        <div>RegisterPurifier</div>
+        <div>
+            This is a page where admins will be able to register purifiers of users
+        </div>
     )
 }
 
